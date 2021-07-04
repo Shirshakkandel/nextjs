2b32391c6700f20d8eaeeb02b93119f999507c86
Shirshakkandel <shirshakkandel@gmail.com>
2021-07-04 21:21:04 +0545

    first commit

---
+++ pages/about.jsx
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Shirshak List | About</title>
+        <meta name="keywords" content="Shirshak" />
+      </Head>
+
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum, odit aspernatur laborum quia sint harum nam aperiam dolorum id! Optio
+          perferendis iure repellat quam, provident voluptas incidunt reiciendis quia ratione.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure exercitationem nihil laudantium reprehenderit, magnam illum earum eaque animi
+          in minus, rem dolor. Odit adipisci fugiat laborum sint ut, nam unde?
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default About
